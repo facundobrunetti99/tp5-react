e9a3c4ecf6a986cad580bb971fc3c22819e42c2a
Facundo Brunetti <facundobrunetti99@gmail.com>
2024-12-06 19:38:10 -0300

    add Content and styles

---
+++ src/components/Content/AppContent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Content/Content.css'
+
+const AppContent = () => {
+  return (
+    <>
+    
+    <div className='main_content'>
+    <h1 className='h1_content_home'>Project Task</h1>
+
+    </div>
+    </>
+  )
+}
+
+export default AppContent
