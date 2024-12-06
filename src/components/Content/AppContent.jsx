a9536225776c1438b5d3530b8866665ccba4d702
Facundo Brunetti <facundobrunetti99@gmail.com>
2024-12-06 20:01:19 -0300

    add Router and Login

--- src/components/Content/AppContent.jsx
+++ src/components/Content/AppContent.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import '../Content/Content.css'
-
+import React from "react";
+import "../Content/Content.css";
+import Navbar from "../Navbar/Navbar";
 const AppContent = () => {
   return (
     <>
-    
-    <div className='main_content'>
-    <h1 className='h1_content_home'>Project Task</h1>
-
-    </div>
+      <Navbar></Navbar>
+      <div className="main_content">
+        <h1 className="h1_content_home">Project Task</h1>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AppContent
+export default AppContent;
